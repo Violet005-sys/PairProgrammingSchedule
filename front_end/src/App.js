da6f4b4b64b04119896ef1984dac64e103402a34
Paul Ndambo <164306210+ndambopaul@users.noreply.github.com>
2024-05-28 11:11:38 +0300

    time slots changes

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TimeSlots from "./components/TimeSlots";
 import NewTimeSlot from './components/NewTimeSlot';
+import SlotDetail from './components/SlotDetail';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" exact element={<TimeSlots />} />
+          <Route path="/slots/:id" element={<SlotDetail />} />
           <Route path="/newslot" exact element={<NewTimeSlot />} />
 
         </Routes>
